2cc840c12ea3a2db306df7a89bbae9df64cf27f0
Robert Reed <robert.mc.reed@gmail.com>
2021-01-04 16:02:32 -0800

    bump commit

--- include/html.h
+++ include/html.h
@@ -12,6 +12,6 @@ const char HTML[] PROGMEM = R"rawliteral(
       <p>If you're seeing this message something has broken.</p>
       <p>Open the console to see if there are any useful errors.</p>
     </div>
-    <script src="https://cdn.jsdelivr.net/gh/RobertMcReed/esp8266_status_light@43015a4eadf6a8defec1cf021bd44ee560262b65/var/www/index.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/RobertMcReed/esp8266_status_light@789c187d6b7c4201966312cb33311f272a1ac6d2/var/www/index.min.js" type="text/javascript"></script>
   </body>
 </html>)rawliteral";
